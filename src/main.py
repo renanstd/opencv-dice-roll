dc0e70c454aac6747d25a61a571a6a490f248df7
Renan Santana <renan_ota@hotmail.com>
2021-02-21 01:34:31 -0300

    Código finalizado

--- src/main.py
+++ src/main.py
@@ -3,6 +3,80 @@ import numpy as np
 from sklearn import cluster
 
 
+def get_blobs(frame):
+    """
+    Identifica os blobs do frame, as 'bolinhas' dos dados, e os
+    retorna em uma lista.
+    """
+    frame_blurred = cv2.medianBlur(frame, 7)
+    frame_gray = cv2.cvtColor(frame_blurred, cv2.COLOR_BGR2GRAY)
+    blobs = detector.detect(frame_gray)
+    return blobs
+
+
+def get_dice_from_blobs(blobs):
+    """
+    A partir de um conjunto de blobs próximos uns aos outros, identifica
+    os dados.
+    """
+    X = []
+    for b in blobs:
+        pos = b.pt
+        if pos != None:
+            X.append(pos)
+    X = np.asarray(X)
+    if len(X) > 0:
+        # Important to set min_sample to 0, as a dice may only have one dot
+        clustering = cluster.DBSCAN(eps=40, min_samples=0).fit(X)
+        # Find the largest label assigned + 1, that's the number of dice found
+        num_dice = max(clustering.labels_) + 1
+        dice = []
+        # Calculate centroid of each dice, the average between all a dice's dots
+        for i in range(num_dice):
+            X_dice = X[clustering.labels_ == i]
+            centroid_dice = np.mean(X_dice, axis=0)
+            dice.append([len(X_dice), *centroid_dice])
+        return dice
+    else:
+        return []
+
+def overlay_info(frame, dice, blobs):
+    """
+    Adiciona ao frame informações visuais dos blobs e resultados obtidos nos
+    dados.
+    """
+    # Overlay blobs
+    for b in blobs:
+        pos = b.pt
+        r = b.size / 2
+        cv2.circle(
+            frame,
+            (int(pos[0]), int(pos[1])),
+            int(r),
+            (255, 0, 0),
+            2
+        )
+    # Overlay dice number
+    for d in dice:
+        # Get textsize for text centering
+        textsize = cv2.getTextSize(
+            str(d[0]),
+            cv2.FONT_HERSHEY_PLAIN,
+            3,
+            2
+        )[0]
+        cv2.putText(
+            frame,
+            str(d[0]),
+            (int(d[1] - textsize[0] / 2),
+            int(d[2] + textsize[1] / 2)),
+            cv2.FONT_HERSHEY_PLAIN,
+            3,
+            (0, 255, 0),
+            2
+        )
+
+
 ip_webcam_url = "http://192.168.1.32:4747/video"
 cap = cv2.VideoCapture(ip_webcam_url)
 
@@ -11,18 +85,22 @@ while(True):
     # Cortar imagem para remover o header do DroidCam
     frame = frame[10:frame.shape[0], 0:frame.shape[1]]
 
-    # blobs = get_blobs(frame)
-    # dice = get_dice_from_blobs(blobs)
-    # out_frame = overlay_info(frame, dice, blobs)
+    # Setup do BlobDetector
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByInertia
+    params.minInertiaRatio = 0.6
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    blobs = get_blobs(frame)
+    # print(blobs)
+    dice = get_dice_from_blobs(blobs)
+    # print(dice)
+    out_frame = overlay_info(frame, dice, blobs)
 
     cv2.imshow("frame", frame)
-
     res = cv2.waitKey(1)
-
-    # Stop if the user presses "q"
     if res & 0xFF == ord('q'):
         break
 
-# When everything is done, release the capture
 cap.release()
 cv2.destroyAllWindows()
